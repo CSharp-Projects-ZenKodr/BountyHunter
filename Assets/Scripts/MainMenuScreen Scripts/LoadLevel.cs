8fbed10e669c37fb54efb71d32196f4a94588f87
Muhammad Shahrom Ali <estineali@gmail.com>
2020-02-19 02:09:48 +0500

    Fade on menu screen

--- Assets/Scripts/MainMenuScreen Scripts/LoadLevel.cs
+++ Assets/Scripts/MainMenuScreen Scripts/LoadLevel.cs
@@ -8,8 +8,37 @@ using UnityEngine.SceneManagement;
     LevelLoader
  */
 
+
+
+
 public class LoadLevel : MonoBehaviour {
 
-    public void Play() { SceneManager.LoadScene(1); } 
+    public Image fade;
+    private bool fade_now, LoadLevelNow;
+
+    private void Update()
+    {
+        if (fade_now && fade.color.a <= 1)
+        {
+            fade.color = new Color(fade.color.r, fade.color.g, fade.color.b, fade.color.a + 0.05f);
+            if (fade.color.a >= 1.0f)
+            {
+                LoadLevelNow = true; 
+                Play();
+            }
+        }
+    }
+
+    public void Play() 
+    {
+        if (LoadLevelNow)
+        {
+            SceneManager.LoadScene(1);
+        }
+        fade.gameObject.SetActive(true);
+        fade_now = true;
+        
+         
+    } 
     public void Quit() { Application.Quit(); }
 }
