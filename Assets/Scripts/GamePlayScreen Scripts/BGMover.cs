7c010b76b48aae3ef6b9d8b4e9256cbfaf2c12b5
Muhammad Shahrom Ali <estineali@gmail.com>
2020-02-25 00:05:29 +0500

    Work In Progress

---
+++ Assets/Scripts/GamePlayScreen Scripts/BGMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform SpawnLoc;
+
+    private void Awake()
+    {
+        SpawnLoc = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.1f);
+
+        //Debug.Log(transform.position);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Inbound");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("Trigger Inbound");
+        Instantiate(gameObject, SpawnLoc);
+    }
+}
